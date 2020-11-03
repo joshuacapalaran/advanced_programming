9af5774c2607ea7c8fa02138d81f1ef57d38a08b
joshuacapalaran <joshuacapalaran27@gmail.com>
2020-11-03 22:48:18 +0800

    Added index.php of print name, calculator and gcd.

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <html>
     <head>
-        <title>Assignment #1</title>
+        <title></title>
     </head>
     <body>
       <b><p>Joshua A. Capalaran</p></b>
       <b><p>CALCULATOR</p></b>
         <form>
-            <input type = "number" name = "num1" placeholder = "Input First Number" required>
+            <input type = "number" name = "cal_num1" placeholder = "Input First Number" required>
             <br><br>
-            <input type = "number" name = "num2" placeholder = "Input Second Number" required>
+            <input type = "number" name = "cal_num2" placeholder = "Input Second Number" required>
             <p>Select an operator: </p>
             <select name = "operator">
                 <option>None</option>
@@ -23,8 +23,8 @@
 
     <?php
        if (isset($_GET['submit'])) {
-           $firstNum = $_GET['num1'];
-           $secondNum = $_GET['num2'];
+           $firstNum = $_GET['cal_num1'];
+           $secondNum = $_GET['cal_num2'];
            $operator = $_GET['operator'];
 
        switch ($operator) {
@@ -38,7 +38,7 @@
                echo $firstNum * $secondNum;
                break;
            case "/":
-               echo $firstNum / $secondNumb;
+               echo $firstNum / $secondNum;
                break;
            default:
                echo "You should select an operator!";
@@ -49,19 +49,18 @@
     <br>
       <b><p>FIND THE GCD</p></b>
       <form action = index.php method="post">
-          <input type="number" name="gcd-num1" placeholder="Input First Number" required><br><br>
-          <input type="number" name="gcd-num2" placeholder="Input Second Number" required><br><br>
+          <input type="number" name="gcd_num1" placeholder="Input First Number" required><br><br>
+          <input type="number" name="gcd_num2" placeholder="Input Second Number" required><br><br>
           <button type="submit">Submit</button>
       </form>
-      <br>
+      <p>The answer is: </p>
       <?php
           $gcd = 1;
           if(!empty($_POST)){
-              $gcd_num1 = $_POST['gcd-num1'];
-              $gcd_num2 = $_POST['gcd-num2'];
-              echo "The answer is: ";
-              for ($i=1; $i<=max($gcd_num1,$gcd_num2);$i++)
-                  if($gcd_num1 % $i == 0 && $gcd_num2 % $i == 0)
+              $num1 = $_POST['gcd_num1'];
+              $num2 = $_POST['gcd_num2'];
+              for ($i=1; $i<=max($num1,$num2);$i++)
+                  if($num1 % $i == 0 && $num2 % $i == 0)
                       $gcd = $i;
               echo $gcd;
           }
