3e8486dab3074b599ce6e8e3ec4a43f58b67e9b6
joshuacapalaran <joshuacapalaran27@gmail.com>
2020-11-03 21:58:57 +0800

    Added index.php of creating a class.

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-echo "assign_two";
-?>
+  class User
+  {
+    public $name;
+    public $username;
+    public $email;
+
+    public function addFriend($name)
+    {
+      echo $name . " added a friend.";
+    }
+
+    public function postStatus($name)
+    {
+      echo $name . " posted a status.";
+    }
+  }
+
+  $user1 = new User();
+  $user1->addFriend("Joshua Capalaran");
+  echo "<br>";
+  $user1->postStatus("Joshua Capalaran");
+ ?>
